d4102494725839a911d969d02fbc9a7d053d4541
elbalfqyasma@gmail.com <email-de-hajar@example.com>
2025-10-29 13:48:01 +0100

    feat: add Log4j2 logging, TestNG cross-browser setup, and config.properties support

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,19 +1,27 @@
 package testBase;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.apache.commons.text.RandomStringGenerator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
 
-	public WebDriver driver;  //Log4j
-	public Logger logger;  //Log4j
+	public WebDriver driver; 
+	public Logger logger; // Log4j
+	public Properties p;
 
 	// ===============================
 	// Setup Method
@@ -21,16 +29,41 @@ public class BaseClass {
 	/**
 	 * Method annotated with @BeforeClass runs once before all test methods in this
 	 * class. Typically used to initialize WebDriver and open the application URL.
+	 * @throws FileNotFoundException 
 	 */
 	@BeforeClass
-	public void setup() {
-
+	@Parameters({ "os", "browser" })
+	public void setup(String os, String br) throws IOException {
+		
+        //Loading config.properties file
+		FileReader file = new FileReader("./src//resources//config.properties");
+		p= new Properties();
+		p.load(file);
+		
 		logger = LogManager.getLogger(this.getClass());
 
-		driver = new ChromeDriver();
+		switch (br.toLowerCase()) {
+		case "chrome":
+			driver = new ChromeDriver();
+			break;
+
+		case "firefox":
+			driver = new FirefoxDriver();
+			break;
+
+		case "edge":
+			driver = new EdgeDriver();
+			break;
+
+		default:
+			System.out.println("Browser not supported");
+			driver = null;
+			return;
+		}
+
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://tutorialsninja.com/demo/");
+		driver.get(p.getProperty("appUrl2"));
 		driver.manage().window().maximize();
 
 	}
